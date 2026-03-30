7719d1bc207c5c5043fa433bfd31797b1525de70
rrrwrr <maw9374@thi.de>
2026-03-30 12:04:25 +0200

    1.

---
+++ MediaPlayer_VA06/src/audioplayer/main/AudioFile.java
@@ -0,0 +1,26 @@
+package audioplayer.main;
+
+import java.io.File;
+
+public class AudioFile {
+	public String pathname;
+	public String filename;
+
+	public void parsePathname(String path) {
+		return ;
+	}
+	
+	public String getPathname() {
+		return pathname;
+	}
+	
+	public String getFilename() {
+		return filename;
+	}
+	
+	public static void main(String[] args) {
+		parsePathname(pathname);
+		
+	}
+
+}
